6f99736fd3a73512eb604ffcf618d038621e1c77
Anara5 <55089741+Anara5@users.noreply.github.com>
2020-04-24 21:41:52 +0200

    Don't know why it doesn't work with tests 2 and 3. Needs proper codes

--- src/busses/Main.java
+++ src/busses/Main.java
@@ -1,8 +1,14 @@
 package busses;
 
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+
     }
 }
--- test/busses/RoutingTests.java
+++ test/busses/RoutingTests.java
@@ -1,6 +1,41 @@
 package busses;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoutingTests {
+
+    @Test
+    void oneNode_zeroDistance() {
+        final BussStopGraph graph = new BussStopGraph("A");
+
+        final int distance = graph.shortestDistanceBetween("A", "A");
+
+        assertEquals(0, distance);
+    }
+
+    @Test
+    void twoNodes_passBetween_equalWeight(){
+        final BussStopGraph graph = new BussStopGraph("A", "B");
+        graph.addNeighbor("A", "B", 1);
+
+        final int distance = graph.shortestDistanceBetween("A", "B");
+
+        assertEquals(1, distance);
+    }
+
+    @Test
+    void threeNodes_passAll_eualSum(){
+        final BussStopGraph graph = new BussStopGraph("A", "B", "C");
+        graph.addNeighbor("A", "B", 1);
+        graph.addNeighbor("B", "C", 3);
+
+        final int distance = graph.shortestDistanceBetween("A","C");
+
+        assertEquals(4, distance);
+    }
 
 
 }
