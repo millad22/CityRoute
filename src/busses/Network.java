bc62044f586a9476e2fd0b6d0f75593cec72bb88
Anara Zhunusova <55089741+Anara5@users.noreply.github.com>
2020-05-17 14:08:48 +0200

    Update Network.java

--- src/busses/Network.java
+++ src/busses/Network.java
@@ -1,35 +1,38 @@
-package busses;
+package routs;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.Random;
 /**
- * the Network to build connection between stations
- * Stations in a char list
- * Network constructor
+ * the names of stations are in form of an char array (A, B, C ect)
+ * a list of stations as object
  */
-class Network {
-    private Station[] stations;
-    Station[] getStations(){
+public class Network {
+    public static Station[] stations;
+
+    Station[] getStations() {
         return this.stations;
     }
-    Network(int stations, int minPaths, int maxPaths) {
+
+    /**
+     * building the network of stations
+     * here is a constructor so we can set up out network details: 10 stops, 2 minimum paths, 3 maximum paths
+     */
+    public Network(int stations, int minPath, int maxPath) {
         this.stations = new Station[stations];
         for (int i = 0; i < stations; i++) {
-            this.stations[i] = new Station((char) ((int)'A' + i));
+            this.stations[i] = new Station((char) ((int) 'A' + i));
         }
-        this.generateRelations(minPaths, maxPaths);
+        this.generateRelations(minPath, maxPath); //call the method generateRelations to connect stations with paths
     }
+
     /**
-     * method to generate relations
-     * @param minPaths = 2
-     * @param maxPaths = 3
+     * method to build relations in the network
      */
-    private void generateRelations(int minPaths, int maxPaths) {
+    public void generateRelations(int minPath, int maxPath) {
         var length = this.stations.length;
         ArrayList<Integer>[] relations = new ArrayList[length];
         ArrayList<Integer>[] available = new ArrayList[length];
-        var notReady = new ArrayList<Integer>(length);
-        var notIntegrated = new ArrayList<Integer>(length);
+        var notReady = new ArrayList<Integer>(length); //the stations is not having at least 2 edges yet
+        var notIntegrated = new ArrayList<Integer>(length); //the stations not connected yet
         for (int i = 0; i < length; i++) {
             notReady.add(i);
             notIntegrated.add(i);
@@ -41,29 +44,29 @@ class Network {
                 }
             }
         }
-        /**
-         * network as an object
-         * amount of relations from 2 to 3
-         */
-        Network network = this;
-        Relator relator = (a, b, weight) -> { //relation object replaced with Lambda
-            relations[a].add(b);
-            network.stations[a].addPath(network.stations[b], weight);
-            if (relations[a].size() == 3) {
-                for (int i=0; i < length; i++) {
-                    available[i].removeIf(x -> x == a);
-                }
-            }
-            else {
-                available[a].removeIf(x -> x == b);
-                if (relations[a].size() == 2) {
-                    notReady.removeIf(x -> x == a);
+
+        var network = this;
+        var relater = new Relater() {
+            public void addPath(Integer a, Integer b, int weight) {
+                relations[a].add(b);
+                network.stations[a].addPath(network.stations[b], weight);
+                if (relations[a].size() == maxPath) {
+                    for (int i = 0; i < length; i++) {
+                        available[i].removeIf(x -> x == a);
+                    }
+                } else {
+                    available[a].removeIf(x -> x == b);
+                    if (relations[a].size() == minPath) {
+                        notReady.removeIf(x -> x == a);
+                    }
                 }
             }
         };
+
         /**
          * random variable for the connection between the stations
          * do while the connections are completed till the circle
+         * listA - bus stops, listB - bus stops connected to the listA
          */
         var random = new Random();
         do {
@@ -76,13 +79,33 @@ class Network {
             }
             var b = listB.get(random.nextInt(listB.size()));
             if (listA == notIntegrated) {
-                listA.removeIf(x -> x.equals(a));
-                listA.removeIf(x -> x.equals(b));
+                listA.removeIf(x -> x == a);
+                listA.removeIf(x -> x == b);
             }
-            var weight = 1 + random.nextInt(10);
-            relator.addPath(a, b, weight);
-            relator.addPath(b, a, weight);
+            int weight = 1 + random.nextInt(10); //each edge weight is from 1 to 10
+            relater.addPath(a, b, weight); //connecting stations, method addPath
+            relater.addPath(b, a, weight);
         }
         while (notReady.size() > 0);
+    }
+
+    //TODO dijkstra algorithm
+
+    /**
+     * implementing Dijkstra algorithm to find the shortest path between the start and end stations
+     * @return
+     */
+    public int shortestPath(Station from, Station to) {
+        
+
+        int distance = 0;
+        return distance;
+    }
+
+    public void addLine(char a, char a1, int cost) {
+    }
+
+    public void getShortestPath(char a, char a1) {
+
     }
 }
